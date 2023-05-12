265c27cdc7b63cbd15af8b509dd78f4e6907a54b
Darrian Chen <107444978+CozyCloud22@users.noreply.github.com>
2023-05-12 18:08:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', stopLoadingBar);
+
+function stopLoadingBar() {
   var loadingBar = document.querySelector('.loading-bar');
 
   loadingBar.addEventListener('animationiteration', function() {
     // Stop the loading bar animation
     loadingBar.style.animationPlayState = 'paused';
 
-    // Trigger the page load when the animation iteration completes
-    window.location.href = 'index.html';
-  });
-});
+    // Fade out the loading bar
+    loadingBar.style.opacity = '0';
 
-function stopLoadingBar() {
-  var loadingBar = document.querySelector('.loading-bar');
-  loadingBar.style.animationPlayState = 'paused';
+    // Remove the loading bar from the DOM after it fades out
+    setTimeout(function() {
+      
+      loadingBar.parentNode.removeChild(loadingBar);
+    }, 500000);
+    //^^adjust the duration (in milliseconds) based on your transition time
+  });
+
+  setTimeout(function() {
+    loadingBar.parentNode.removeChild(loadingBar);
+  }, 500000);
 }
 
-var stopTimeout = setTimeout(stopLoadingBar, 2000);
+//var stopTimeout = setTimeout(stopLoadingBar, 2000);
+
+window.addEventListener('load', function() {
+  var background = document.querySelector('.background');
+  
+  // Add the "show" class after a delay to trigger the opacity transition
+  setTimeout(function() {
+    background.classList.add('show');
+  }, 1000); // Adjust the delay (in milliseconds) based on your needs
+});
