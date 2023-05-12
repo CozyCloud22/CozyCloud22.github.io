be67d1cb1c62a7c7bd463fb7f2cdb2a49e2de2b4
Darrian Chen <107444978+CozyCloud22@users.noreply.github.com>
2023-05-12 04:48:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 window.addEventListener('load', function() {
   var loadingBar = document.querySelector('.loading-bar');
 
-  // Get the duration of the loading bar animation
-  var animationDuration = parseFloat(getComputedStyle(loadingBar).animationDuration) * 1000;
-
-  // Wait for the animation to finish and then load the homepage
-  setTimeout(function() {
+  loadingBar.addEventListener('animationiteration', function() {
+    // Trigger the page load when the animation iteration completes
     window.location.href = 'index.html';
-  }, animationDuration);
+  });
 });
