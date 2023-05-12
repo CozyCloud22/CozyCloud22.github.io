d021566bd64c3cc73a74ea5f251506c2be2d3a6b
Darrian Chen <107444978+CozyCloud22@users.noreply.github.com>
2023-05-12 06:16:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ window.addEventListener('load', function() {
   var loadingBar = document.querySelector('.loading-bar');
 
   loadingBar.addEventListener('animationiteration', function() {
+    // Stop the loading bar animation
+    loadingBar.style.animationPlayState = 'paused';
+
     // Trigger the page load when the animation iteration completes
     window.location.href = 'index.html';
   });
 });
 
 function stopLoadingBar() {
-  clearTimeout(redirectTimeout);
-  clearInterval(stopTimeout);
+  var loadingBar = document.querySelector('.loading-bar');
+  loadingBar.style.animationPlayState = 'paused';
 }
+
+var stopTimeout = setTimeout(stopLoadingBar, 2000);
